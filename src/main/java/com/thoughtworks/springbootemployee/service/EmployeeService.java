4d7b68c4a6f4e281e95151ec9a0861641654a7b6
Domingo <986224568@qq.com>
2020-07-28 17:01:07 +0800

    [spring-boot-employee][Domingo] feature add EmployeeService

---
+++ src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
@@ -0,0 +1,8 @@
+package com.thoughtworks.springbootemployee.service;
+
+public interface EmployeeService {
+    void addEmployee();
+    void deleteEmployee();
+    void updateEmployee();
+    void queryEmployee();
+}
